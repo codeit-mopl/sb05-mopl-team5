de7bd6ecb49fc0e49bbc426662beaa181db820f9
parkjoohwan <jookid94@gmail.com>
2025-12-29 11:34:00 +0900

    chore : entity 어노테이션 정리

--- src/main/java/com/mopl/api/global/common/entity/BaseDeletableEntity.java
+++ src/main/java/com/mopl/api/global/common/entity/BaseDeletableEntity.java
@@ -1,12 +1,9 @@
 package com.mopl.api.global.common.entity;
 
 import jakarta.persistence.MappedSuperclass;
-import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @MappedSuperclass
 public class BaseDeletableEntity extends BaseUpdatableEntity {
 
--- src/main/java/com/mopl/api/global/common/entity/BaseEntity.java
+++ src/main/java/com/mopl/api/global/common/entity/BaseEntity.java
@@ -7,14 +7,11 @@ import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.PrePersist;
 import java.time.LocalDateTime;
 import java.util.UUID;
-import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
