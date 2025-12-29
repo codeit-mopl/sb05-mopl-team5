fe329b9c7fe5dfae93e555c7bedea537e55e9845
parkjoohwan <jookid94@gmail.com>
2025-12-29 13:17:13 +0900

    chore : controller ex

--- src/main/java/com/mopl/api/domain/user/controller/UserController.java
+++ src/main/java/com/mopl/api/domain/user/controller/UserController.java
@@ -1,10 +1,6 @@
 package com.mopl.api.domain.user.controller;
 
-import com.mopl.api.domain.user.dto.WatchingSessionDto;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,10 +9,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 public class UserController {
 
-    @Tag(name = "시청 세션 관리", description = "시청 세션 관리 API")
-    @GetMapping("/{watcherId}/watching-sessions")
-    public ResponseEntity<WatchingSessionDto> getWatchingSession(Long watcherId, Long contentId) {
-        return ResponseEntity.ok(WatchingSessionDto.builder()
-                                                   .build());
-    }
 }
