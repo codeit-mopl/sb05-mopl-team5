7ac3dacbcc3c056e670d57e5b630d70cb6e782bc
parkjoohwan <jookid94@gmail.com>
2025-12-29 13:13:59 +0900

    chore : repository custom 예시, SecurityConfig 인증 비활성화
    remove : 불필요 파일 제거

--- src/main/java/com/mopl/api/domain/user/controller/UserController.java
+++ src/main/java/com/mopl/api/domain/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.mopl.api.domain.user.controller;
 
 import com.mopl.api.domain.user.dto.WatchingSessionDto;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 public class UserController {
 
+    @Tag(name = "시청 세션 관리", description = "시청 세션 관리 API")
     @GetMapping("/{watcherId}/watching-sessions")
     public ResponseEntity<WatchingSessionDto> getWatchingSession(Long watcherId, Long contentId) {
         return ResponseEntity.ok(WatchingSessionDto.builder()
