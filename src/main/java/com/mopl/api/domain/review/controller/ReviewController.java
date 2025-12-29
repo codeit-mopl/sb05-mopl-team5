06f40383e0fe08ce39d6566064d551b4d8290668
yuhandemian <yuhn1011@naver.com>
2025-12-29 18:56:52 +0900

    refactor: patch 컨트롤러 오타 수정

--- src/main/java/com/mopl/api/domain/review/controller/ReviewController.java
+++ src/main/java/com/mopl/api/domain/review/controller/ReviewController.java
@@ -12,9 +12,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,10 +33,11 @@ public class ReviewController {
         @Valid @RequestBody ReviewCreateRequest request,
         @RequestHeader("X-User-Id") UUID userId
     ) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(reviewService.addReview(request, userId));
+        return ResponseEntity.status(HttpStatus.CREATED)
+                             .body(reviewService.addReview(request, userId));
     }
 
-    @PutMapping("/{reviewId}")
+    @PatchMapping("/{reviewId}")
     public ResponseEntity<ReviewDto> reviewModify(
         @PathVariable UUID reviewId,
         @Valid @RequestBody ReviewUpdateRequest request,
@@ -51,7 +52,8 @@ public class ReviewController {
         @RequestHeader("X-User-Id") UUID userId
     ) {
         reviewService.removeReview(reviewId, userId);
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.noContent()
+                             .build();
     }
 
     @GetMapping
