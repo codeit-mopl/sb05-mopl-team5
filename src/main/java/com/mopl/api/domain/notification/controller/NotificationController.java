b380663b5bcea6a7f01f523e3372d6a36540405c
polodumbo <polosh1208@gmail.com>
2025-12-30 01:43:22 +0900

    feat: notification API 엔드포인트 활성화

---
+++ src/main/java/com/mopl/api/domain/notification/controller/NotificationController.java
@@ -0,0 +1,42 @@
+package com.mopl.api.domain.notification.controller;
+
+import com.mopl.api.domain.notification.dto.NotificationDto;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+GET
+/api/notifications
+알림 목록 조회 (커서 페이지네이션)
+
+DELETE
+/api/notifications/{notificationId}
+알림 읽음 처리
+ */
+
+@Tag(name = "알림", description = "알림 API")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/notifications")
+public class NotificationController {
+
+    @GetMapping
+    public ResponseEntity<NotificationDto> getNotification(UUID userI) {
+        // TODO ...
+        return ResponseEntity.ok(NotificationDto.builder()
+                                                .build());
+    }
+
+    @DeleteMapping("/{NotificationId}")
+    public ResponseEntity<NotificationDto> deleteNotification(UUID notificationId) {
+        // TODO ...
+        return ResponseEntity.noContent()
+                             .build();
+    }
+}
