6a22ea52842d9326f29ce3b771a57d3aeb61ed3d
parkjoohwan <jookid94@gmail.com>
2025-12-31 15:15:26 +0900

    chore : content 유니크 제약조건

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -46,7 +46,7 @@ CREATE TABLE `contents`
     `title`          VARCHAR(255)  NOT NULL,
     `description`    VARCHAR(1000) NOT NULL,
     `thumbnail_url`  VARCHAR(255)  NOT NULL,
-    `tags`           VARCHAR(500) NOT NULL,
+    `tags`           VARCHAR(500)  NOT NULL,
     `average_rating` DECIMAL(2, 1) NOT NULL DEFAULT 0.0,
     `review_count`   BIGINT        NOT NULL DEFAULT 0,
     `watcher_count`  BIGINT        NOT NULL DEFAULT 0,
@@ -54,7 +54,8 @@ CREATE TABLE `contents`
     `updated_at`     DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     `is_deleted`     BOOLEAN       NOT NULL DEFAULT FALSE,
 
-    CONSTRAINT `PK_CONTENTS` PRIMARY KEY (`id`)
+    CONSTRAINT `PK_CONTENTS` PRIMARY KEY (`id`),
+    CONSTRAINT `UK_CONTENTS_TYPE_API_ID` UNIQUE (`type`, `api_id`)
 );
 
 -- ==========================================
