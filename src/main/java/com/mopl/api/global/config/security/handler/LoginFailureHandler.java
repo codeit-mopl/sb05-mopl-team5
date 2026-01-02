198570f8291e78dcbd0c44dd6f14de174c4e2819
yelim-Lee <dpfla1078@naver.com>
2026-01-02 16:32:26 +0900

    feat : jwt handler 구현

---
+++ src/main/java/com/mopl/api/global/config/security/handler/LoginFailureHandler.java
@@ -0,0 +1,39 @@
+package com.mopl.api.global.config.security.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class LoginFailureHandler implements AuthenticationFailureHandler {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        AuthenticationException exception)
+        throws IOException, ServletException {
+        log.error("Authentication Failure : {}", exception.getMessage(), exception);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+
+        objectMapper.writeValue(response.getWriter(), new FailureBody(
+            "UNAUTHORIZED",
+            "아이디 또는 비밀번호가 올바르지 않습니다."
+        ));
+    }
+
+    private record FailureBody(String code, String message){}
+}
