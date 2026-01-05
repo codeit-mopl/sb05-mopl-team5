ce99d33c385d29e11da40d010f3358c7848d9033
j2eun0922 <j2eun0922@naver.com>
2026-01-05 17:27:05 +0900

    feat: 인가 어노테이션 추가 및 파일 파라미터 @RequestPart로 변경

--- src/main/java/com/mopl/api/domain/content/controller/ContentController.java
+++ src/main/java/com/mopl/api/domain/content/controller/ContentController.java
@@ -11,14 +11,14 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -45,22 +45,25 @@ public class ContentController {
     }
 
     @PostMapping
-    public ResponseEntity<ContentDto> contentAdd(@Valid @RequestBody ContentCreateRequest request,
-        @RequestParam MultipartFile file) {
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<ContentDto> contentAdd(@Valid @RequestPart ContentCreateRequest request,
+        @RequestPart MultipartFile file) {
         ContentDto contentDto = contentService.addContent(request, file);
         return ResponseEntity.status(HttpStatus.CREATED)
                              .body(contentDto);
     }
 
     @PatchMapping("/{contentId}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ContentDto> contentModify(@PathVariable UUID contentId,
-        @Valid @RequestBody ContentUpdateRequest request, @RequestParam(required = false) MultipartFile file) {
+        @Valid @RequestPart ContentUpdateRequest request, @RequestPart(required = false) MultipartFile file) {
         ContentDto contentDto = contentService.modifyContent(contentId, request, file);
         return ResponseEntity.status(HttpStatus.OK)
                              .body(contentDto);
     }
 
     @DeleteMapping("/{contentId}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ContentDto> contentRemove(@PathVariable UUID contentId) {
         contentService.removeContent(contentId);
         return ResponseEntity.status(HttpStatus.NO_CONTENT)
