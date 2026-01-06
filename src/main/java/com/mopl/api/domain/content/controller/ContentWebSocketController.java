7b7a1de7e241d94805cd7ee45b44d3503f00b02a
parkjoohwan <jookid94@gmail.com>
2026-01-06 09:09:25 +0900

    feat : ws endpoint 설정

---
+++ src/main/java/com/mopl/api/domain/content/controller/ContentWebSocketController.java
@@ -0,0 +1,81 @@
+package com.mopl.api.domain.content.controller;
+
+import com.mopl.api.domain.content.service.ContentChatService;
+import com.mopl.api.domain.user.dto.command.WatchingSessionCreateCommand;
+import com.mopl.api.domain.user.service.WatchingSessionService;
+import com.mopl.api.global.config.websocket.dto.ContentChatSendRequest;
+import com.mopl.api.global.config.websocket.dto.WatchingSessionChange;
+import com.mopl.api.global.config.websocket.dto.WatchingSessionChange.ChangeType;
+import com.mopl.api.global.config.websocket.publisher.WatchingSessionPublisher;
+import jakarta.validation.Valid;
+import java.security.Principal;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ContentWebSocketController {
+
+    private final ContentChatService contentChatService;
+    private final WatchingSessionService watchingSessionService;
+    private final WatchingSessionPublisher publisher;
+
+    @PostMapping("/test/ws/{contentId}")
+    public void test(@PathVariable UUID contentId) {
+        // TODO webocket연결 및 redis 브로커 동작 확인용, 추후 삭제
+        publisher.publish(
+            contentId,
+            new WatchingSessionChange(ChangeType.JOIN, null, 1)
+        );
+    }
+
+    /**
+     * 콘텐츠 채팅 전송
+     * Client: SEND /pub/contents/{contentId}/chat
+     */
+    @MessageMapping("/contents/{contentId}/chat")
+    public void sendChat(
+        @DestinationVariable UUID contentId,
+        @Payload @Valid ContentChatSendRequest request,
+        Principal principal
+    ) {
+        contentChatService.sendChat();
+    }
+
+    /**
+     * 시청 세션 입장 (JOIN)
+     * Client: SEND /pub/contents/{contentId}/watch/join
+     */
+    @MessageMapping("/contents/{contentId}/watch/join")
+    public void joinWatchingSession(
+        @DestinationVariable UUID contentId,
+        Principal principal
+    ) {
+        watchingSessionService.addWatchingSession(
+            WatchingSessionCreateCommand.builder()
+                                        .contentId(contentId)
+                                        .watcherId(UUID.fromString(principal.getName()))
+                                        .build()
+        );
+    }
+
+    /**
+     * 시청 세션 퇴장 (LEAVE)
+     * Client: SEND /pub/contents/{contentId}/watch/leave
+     */
+    @MessageMapping("/contents/{contentId}/watch/leave")
+    public void leaveWatchingSession(
+        @DestinationVariable UUID contentId,
+        Principal principal
+    ) {
+        watchingSessionService.removeWatchingSession(
+            contentId
+        );
+    }
+}
