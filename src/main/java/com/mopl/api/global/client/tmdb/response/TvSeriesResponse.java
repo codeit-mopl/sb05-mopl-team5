23bfac712b33f6f06b7b5034b0102fc6437ab34f
j2eun0922 <j2eun0922@naver.com>
2026-01-06 17:05:13 +0900

    feat: tmdb response Dto 추가

---
+++ src/main/java/com/mopl/api/global/client/tmdb/response/TvSeriesResponse.java
@@ -0,0 +1,33 @@
+package com.mopl.api.global.client.tmdb.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public record TvSeriesResponse(
+    @JsonProperty("id")
+    Long apiId,
+
+    @JsonProperty("original_name")
+    String title,
+
+    @JsonProperty("overview")
+    String description,
+
+    @JsonProperty("poster_path")
+    String thumbnailUrl,
+
+    @JsonProperty("genres")
+    List<TagDto> tags
+) {
+
+    public TvSeriesResponse {
+        thumbnailUrl = thumbnailUrl == null ? null : "https://image.tmdb.org/t/p/w500" + thumbnailUrl;
+    }
+
+    public String getTag() {
+        return tags == null ? null : tags.stream()
+                                         .map(TagDto::name)
+                                         .collect(Collectors.joining("|"));
+    }
+}
