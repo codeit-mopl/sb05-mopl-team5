2ebc9e3b0ded07218a94f37cd3afeeda98bcfe48
parkjoohwan <jookid94@gmail.com>
2026-01-06 09:09:58 +0900

    feat : ContentChatService 스켈레톤

---
+++ src/main/java/com/mopl/api/domain/content/service/ContentChatServiceImpl.java
@@ -0,0 +1,9 @@
+package com.mopl.api.domain.content.service;
+
+public class ContentChatServiceImpl implements ContentChatService {
+
+    @Override
+    public void sendChat() {
+        // TODO chat service 고민 필요
+    }
+}
