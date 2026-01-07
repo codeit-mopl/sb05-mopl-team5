be3081280ded99f341bdc9eb6f93b3de06d3bdad
parkjoohwan <jookid94@gmail.com>
2026-01-07 14:49:27 +0900

    fix : websocket 인증 및 유저 정보 세팅 보완 및 예외

--- src/main/java/com/mopl/api/global/config/websocket/WebSocketAuthChannelInterceptor.java
+++ src/main/java/com/mopl/api/global/config/websocket/WebSocketAuthChannelInterceptor.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageDeliveryException;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
@@ -27,28 +28,22 @@ public class WebSocketAuthChannelInterceptor implements ChannelInterceptor {
 
         if (accessor != null && StompCommand.CONNECT.equals(accessor.getCommand())) {
             String authHeader = accessor.getFirstNativeHeader("Authorization");
+            if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                try {
+                    String token = authHeader.substring(7);
+                    UUID userId = jwtTokenProvider.getUserIdFromToken(token);
 
-            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-                log.error("웹소켓 연결 실패: Authorization 헤더가 없거나 잘못되었습니다.");
-                return message;
-            }
+                    Authentication authentication = new UsernamePasswordAuthenticationToken(
+                        userId.toString(), null, List.of());
 
-            String token = authHeader.substring(7);
-
-            try {
-                UUID userId = jwtTokenProvider.getUserIdFromToken(token);
-
-                Authentication authentication = new UsernamePasswordAuthenticationToken(
-                    userId.toString(),
-                    null,
-                    List.of()
-                );
-
-                accessor.setUser(authentication);
-                log.info("웹소켓 인증 성공: 유저 ID = {}", userId);
-
-            } catch (Exception e) {
-                log.error("웹소켓 인증 실패: {}", e.getMessage());
+                    accessor.setUser(authentication);
+                } catch (Exception e) {
+                    log.error("웹소켓 인증 실패: {}", e.getMessage());
+                    throw new MessageDeliveryException("인증 실패");
+                }
+            } else {
+                log.error("Authorization 헤더가 없습니다.");
+                throw new MessageDeliveryException("헤더 누락");
             }
         }
 
