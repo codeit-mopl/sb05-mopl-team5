d46d2c7d5c38cec10943d817957575ed376a75e6
parkjoohwan <jookid94@gmail.com>
2026-01-07 09:43:06 +0900

    remove : 불필요 import 제거

--- src/main/java/com/mopl/api/MoplApplication.java
+++ src/main/java/com/mopl/api/MoplApplication.java
@@ -2,9 +2,7 @@ package com.mopl.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
