bf50147139d4d267fd0fbd03d4e5c6ed2abe84b6
j2eun0922 <j2eun0922@naver.com>
2026-01-07 10:34:53 +0900

    feat: tmdbApiClient 구현

---
+++ src/main/java/com/mopl/api/global/client/tmdb/TmdbApiClient.java
@@ -0,0 +1,103 @@
+package com.mopl.api.global.client.tmdb;
+
+import com.mopl.api.global.client.tmdb.response.Genre;
+import com.mopl.api.global.client.tmdb.response.GenreResponse;
+import com.mopl.api.global.client.tmdb.response.MovieResponse;
+import com.mopl.api.global.client.tmdb.response.PopularMovieResponse;
+import com.mopl.api.global.client.tmdb.response.PopularTvSeriesResponse;
+import com.mopl.api.global.client.tmdb.response.TvSeriesResponse;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Slf4j
+@Component
+public class TmdbApiClient {
+
+    @Value("${tmdb.api-key}")
+    private String apiKey;
+
+    private final RestClient restClient;
+
+    public TmdbApiClient(RestClient.Builder builder) {
+        this.restClient = builder
+            .baseUrl("https://api.themoviedb.org/3")
+            .build();
+    }
+
+    public List<MovieResponse> getMovieDetails(Long page) {
+        try {
+            PopularMovieResponse popularMovies = restClient.get()
+                                                           .uri(uriBuilder ->
+                                                               uriBuilder.path("/movie/popular")
+                                                                         .queryParam("page", page)
+                                                                         .queryParam("api_key", apiKey)
+                                                                         .queryParam("language", "ko-KR")
+                                                                         .build()
+                                                           )
+                                                           .retrieve()
+                                                           .body(PopularMovieResponse.class);
+            return popularMovies != null ? popularMovies.movies() : List.of();
+        } catch (Exception e) {
+            log.warn("Failed to fetch movie from TMDB for page=" + page, e);
+            return List.of();
+        }
+    }
+
+    public List<Genre> getMovieGenres() {
+        try {
+            GenreResponse genres = restClient.get()
+                                             .uri(uriBuilder ->
+                                                 uriBuilder.path("/genre/movie/list")
+                                                           .queryParam("api_key", apiKey)
+                                                           .queryParam("language", "ko-KR")
+                                                           .build()
+                                             )
+                                             .retrieve()
+                                             .body(GenreResponse.class);
+            return genres != null ? genres.genres() : List.of();
+        } catch (Exception e) {
+            log.warn("Failed to fetch movie from TMDB for genre=", e);
+            return List.of();
+        }
+    }
+
+    public List<TvSeriesResponse> getTvSeriesDetails(Long page) {
+        try {
+            PopularTvSeriesResponse popularTvSeries = restClient.get()
+                                                                .uri(uriBuilder ->
+                                                                    uriBuilder.path("/tv/popular")
+                                                                              .queryParam("page", page)
+                                                                              .queryParam("api_key", apiKey)
+                                                                              .queryParam("language", "ko-KR")
+                                                                              .build()
+                                                                )
+                                                                .retrieve()
+                                                                .body(PopularTvSeriesResponse.class);
+            return popularTvSeries != null ? popularTvSeries.tvSeries() : List.of();
+        } catch (Exception e) {
+            log.warn("Failed to fetch tvSeries from TMDB for page=" + page, e);
+            return List.of();
+        }
+    }
+
+    public List<Genre> getTvGenres() {
+        try {
+            GenreResponse genres = restClient.get()
+                                             .uri(uriBuilder ->
+                                                 uriBuilder.path("/genre/tv/list")
+                                                           .queryParam("api_key", apiKey)
+                                                           .queryParam("language", "ko-KR")
+                                                           .build()
+                                             )
+                                             .retrieve()
+                                             .body(GenreResponse.class);
+            return genres != null ? genres.genres() : List.of();
+        } catch (Exception e) {
+            log.warn("Failed to fetch movie from TMDB for genre=", e);
+            return List.of();
+        }
+    }
+}
