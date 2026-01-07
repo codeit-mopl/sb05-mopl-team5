4e025437fd35f7db30436e0efcd4a80ae3549ee3
yuhandemian <yuhn1011@naver.com>
2026-01-07 13:33:48 +0900

    refactor: SubscriptionService 예외 처리 개선 및 구현 완성

--- src/main/java/com/mopl/api/domain/playlist/repository/SubscriptionRepository.java
+++ src/main/java/com/mopl/api/domain/playlist/repository/SubscriptionRepository.java
@@ -1,10 +1,13 @@
 package com.mopl.api.domain.playlist.repository;
 
 import com.mopl.api.domain.playlist.entity.Subscription;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SubscriptionRepository extends JpaRepository<Subscription, UUID> {
+
+    Optional<Subscription> findByUserIdAndPlaylistId(UUID userId, UUID playlistId);
 
     boolean existsByUserIdAndPlaylistId(UUID userId, UUID playlistId);
 }
