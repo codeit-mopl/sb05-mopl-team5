a77978ddd0699d9a49bd6fbed7e2eeacd221f532
parkjoohwan <jookid94@gmail.com>
2026-01-09 15:32:40 +0900

    refactor : user mapper로 정리

--- src/main/java/com/mopl/api/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/mopl/api/domain/user/service/UserServiceImpl.java
@@ -123,6 +123,6 @@ public class UserServiceImpl implements UserService {
             user.changeProfileImageUrl(imageUrl);
         }
 
-        return UserDto.from(user);
+        return userMapper.toDto(user);
     }
 }
