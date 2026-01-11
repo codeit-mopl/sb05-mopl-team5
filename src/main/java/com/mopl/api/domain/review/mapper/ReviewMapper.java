701b1133b504fdd659ed537a8accfec6d1d11e39
yuhandemian <yuhn1011@naver.com>
2026-01-12 06:16:58 +0900

    refactor: null체크 추가

--- src/main/java/com/mopl/api/domain/review/mapper/ReviewMapper.java
+++ src/main/java/com/mopl/api/domain/review/mapper/ReviewMapper.java
@@ -3,6 +3,7 @@ package com.mopl.api.domain.review.mapper;
 import com.mopl.api.domain.review.dto.response.AuthorDto;
 import com.mopl.api.domain.review.dto.response.ReviewDto;
 import com.mopl.api.domain.review.entity.Review;
+import com.mopl.api.domain.user.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -11,17 +12,20 @@ public interface ReviewMapper {
 
     @Mapping(target = "id", source = "review.id")
     @Mapping(target = "contentId", source = "review.content.id")
-    @Mapping(target = "author", expression = "java(mapAuthor(review))")
+    @Mapping(target = "author", source = "review.user")
     @Mapping(target = "text", source = "review.text")
     @Mapping(target = "rating", source = "review.rating")
     @Mapping(target = "isAuthor", source = "isAuthor")
     ReviewDto toDto(Review review, boolean isAuthor);
 
-    default AuthorDto mapAuthor(Review review) {
+    default AuthorDto map(User user) {
+        if (user == null) {
+            return null;
+        }
         return AuthorDto.builder()
-            .userId(review.getUser().getId())
-            .name(review.getUser().getName())
-            .profileImageUrl(review.getUser().getProfileImageUrl())
+            .userId(user.getId())
+            .name(user.getName())
+            .profileImageUrl(user.getProfileImageUrl())
             .build();
     }
 }
