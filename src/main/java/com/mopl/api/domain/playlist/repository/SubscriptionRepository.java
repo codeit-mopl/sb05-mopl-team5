407bfac5982f7c8a00a2690067d10a279342c3b1
polodumbo <polosh1208@gmail.com>
2026-01-15 11:52:23 +0900

    feat: findSubscriptionsByPlaylistId() 추가

--- src/main/java/com/mopl/api/domain/playlist/repository/SubscriptionRepository.java
+++ src/main/java/com/mopl/api/domain/playlist/repository/SubscriptionRepository.java
@@ -1,11 +1,12 @@
 package com.mopl.api.domain.playlist.repository;
 
 import com.mopl.api.domain.playlist.entity.Subscription;
+import com.mopl.api.domain.playlist.repository.impl.SubscriptionRepositoryCustom;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SubscriptionRepository extends JpaRepository<Subscription, UUID> {
+public interface SubscriptionRepository extends JpaRepository<Subscription, UUID>, SubscriptionRepositoryCustom {
 
     Optional<Subscription> findByUserIdAndPlaylistId(UUID userId, UUID playlistId);
 
